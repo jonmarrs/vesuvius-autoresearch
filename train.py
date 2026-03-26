b9e9f460818adeae1901e2cf5723082f06a42631
Jon Marrs <jdmarrs@gmail.com>
2026-03-26 10:29:24 -0700

    Configure Day Shift loop (7:00 AM - 7:00 PM) and update Transformer training configs

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ class TrainConfig:
     val_uri: str = 'local_data/PHercParis2Fr143/surface_volume/'
 
     batch_size: int = 2 # Minimum to avoid OOM
-    patch_size: int = 128
+    patch_size: int = 96
     num_layers: int = 16 # Must be multiple of 4 for patch_embed
 
     lr: float = 3e-4
