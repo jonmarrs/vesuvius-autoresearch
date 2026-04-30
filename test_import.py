0400b760b7967e15dc2a7e2a0490f92d175c0473
Jon Marrs <jdmarrs@gmail.com>
2026-04-30 13:45:02 -0700

    Handle optional skeleton metric import

--- test_import.py
+++ test_import.py
@@ -7,5 +7,8 @@ print("Torch imported")
 sys.path.append(os.path.abspath('villa/segmentation/evaluation'))
 from metrics.dice import compute as compute_official_dice
 print("Dice imported")
-from metrics.skeleton_distance_length import compute as compute_skeleton_dist
-print("Skel imported")
+try:
+    from metrics.skeleton_distance_length import compute as compute_skeleton_dist
+    print("Skel imported")
+except ImportError as exc:
+    print(f"Skel optional import skipped: {exc}")
