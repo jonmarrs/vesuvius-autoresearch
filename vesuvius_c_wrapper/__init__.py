619a7b4bd5f040f80328fe950bbb8360c56550b1
Jon Marrs <jdmarrs@gmail.com>
2026-06-05 01:23:02 -0700

    refactor: drop vesuvius-c wrapper in favor of volume-cartographer

--- vesuvius_c_wrapper/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility hooks for the official ScrollPrize/villa Vesuvius-C code."""
-
-from .vesuvius_c import FastLocalVolume, VesuviusCUnavailable, VesuviusVolume
-
-__all__ = ["FastLocalVolume", "VesuviusCUnavailable", "VesuviusVolume"]
